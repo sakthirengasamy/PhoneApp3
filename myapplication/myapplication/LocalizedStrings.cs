cd49158882fb308a3f06145a67bd6868233471d5
sakthirengasamy <sakthibalan9393@gmail.com>
2015-02-17 10:02:14 +0530

    hello

---
+++ myapplication/myapplication/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using myapplication.Resources;
+
+namespace myapplication
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
