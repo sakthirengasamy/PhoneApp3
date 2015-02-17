3499167ae13563725a32fef327c371eabfc75606
sakthirengasamy <sakthibalan9393@gmail.com>
2015-02-17 09:49:42 +0530

    my windows app

---
+++ myapp1/myapp1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using myapp1.Resources;
+
+namespace myapp1
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
